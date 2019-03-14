bc741e2732a784eda2a02bb53c4994fe59bd017c
Andrew Turner <andrew.t@brainlabsdigital.com>
2019-03-14 09:27:18 +0000

    value stack

--- vm.h
+++ vm.h
@@ -4,10 +4,15 @@
 #include <stdio.h>
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
     Chunk* chunk;
     uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
 } VM;
 
 typedef enum {
@@ -20,5 +25,8 @@ void initVM();
 void freeVM();
 
 InterpretResult interpret(Chunk* chunk);
+
+void push(Value value);
+Value pop();
 
 #endif
