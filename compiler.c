1bbb9e4b3e39ab42f3853606f323c7a75289a4f9
Andrew Turner <andrew.t@brainlabsdigital.com>
2019-04-26 11:11:17 +0100

    globals

--- compiler.c
+++ compiler.c
@@ -266,6 +266,20 @@ static void string() {
     emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
+static uint8_t identifierConstant(Token* name) {
+    return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static void namedVariable(Token name) {
+    int arg = identifierConstant(&name);
+
+    emitBytes(OP_GET_GLOBAL, (uint8_t)arg);
+}
+
+static void variable() {
+    namedVariable(parser.previous);
+}
+
 static void unary()
 {
     TokenType operatorType = parser.previous.type;
@@ -306,7 +320,7 @@ ParseRule rules[] = {
     {NULL, binary, PREC_COMPARISON}, // TOKEN_GREATER_EQUAL
     {NULL, binary, PREC_COMPARISON}, // TOKEN_LESS
     {NULL, binary, PREC_COMPARISON}, // TOKEN_LESS_EQUAL
-    {NULL, NULL, PREC_NONE},         // TOKEN_IDENTIFIER
+    {variable, NULL, PREC_NONE},         // TOKEN_IDENTIFIER
     {string, NULL, PREC_NONE},       // TOKEN_STRING
     {number, NULL, PREC_NONE},       // TOKEN_NUMBER
     {NULL, NULL, PREC_AND},          // TOKEN_AND
@@ -347,6 +361,15 @@ static void parsePrecedence(Precedence precedence)
     }
 }
 
+static uint8_t parseVariable(const char* errorMessage) {
+    consume(TOKEN_IDENTIFIER, errorMessage);
+    return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global) {
+    emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
 static ParseRule* getRule(TokenType type)
 {
     return &rules[type];
@@ -355,6 +378,19 @@ static ParseRule* getRule(TokenType type)
 void expression()
 {
     parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void varDeclaration() {
+    uint8_t global = parseVariable("Expect variable name.");
+
+    if (match(TOKEN_EQUAL)) {
+        expression();
+    } else {
+        emitByte(OP_NIL);
+    }
+    consume(TOKEN_SEMICOLON, "Expect ';' after variable declaration.");
+
+    defineVariable(global);
 }
 
 static void expressionStatement() {
@@ -369,8 +405,44 @@ static void printStatement() {
     emitByte(OP_PRINT);
 }
 
+static void synchronize() {
+    parser.panicMode = false;
+
+    while (parser.current.type != TOKEN_EOF) {
+        if (parser.previous.type == TOKEN_SEMICOLON) {
+            return;
+        }
+
+        switch (parser.current.type) {
+            case TOKEN_CLASS:
+            case TOKEN_FUN:
+            case TOKEN_VAR:
+            case TOKEN_FOR:
+            case TOKEN_IF:
+            case TOKEN_WHILE:
+            case TOKEN_PRINT:
+            case TOKEN_RETURN: {
+                return;
+            }
+            default: {
+                // Pass
+            }
+        }
+
+        advance();
+    }
+}
+
 static void declaration() {
-    statement();
+    if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        statement();
+    }
+
+    if (parser.panicMode) {
+        synchronize();
+    }
 }
 
 static void statement() {
--- vm.c
+++ vm.c
@@ -31,10 +31,12 @@ static void runtimeError(const char* format, ...) {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM() {
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
     freeObjects();
 }
@@ -74,6 +76,7 @@ static void concatenate() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 
 #define BINARY_OP(valueType, op) \
     do { \
@@ -119,6 +122,22 @@ static InterpretResult run() {
                 break;
             }
             case OP_POP: {
+                pop();
+                break;
+            }
+            case OP_GET_GLOBAL: {
+                ObjString* name = READ_STRING();
+                Value value;
+                if (!tableGet(&vm.globals, name, &value)) {
+                    runtimeError("Undefined variable '%s'.", name->chars);
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(value);
+                break;
+            }
+            case OP_DEFINE_GLOBAL: {
+                ObjString* name = READ_STRING();
+                tableSet(&vm.globals, name, peek(0));
                 pop();
                 break;
             }
@@ -187,6 +206,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE 
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINARY_OP
 }
 
